9ab495b817e4d67a47458e2c90d8abb10d172cec
Raiser00 <damien.khashan@ifosup.wavre.be>
2025-08-08 19:00:42 +0200

    init app.js

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// les routes
+
+app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/users', require('./routes/user.routes'));
+app.use('/api/markets', require('./routes/market.routes'));
+app.use('/api/applications', require('./routes/application.routes'));
+
+module.exports = app;
