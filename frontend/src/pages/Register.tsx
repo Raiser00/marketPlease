2e0e0aaba98524cddcf68a96a6311bfbd720e8d4
Raiser00 <damien.khashan@ifosup.wavre.be>
2025-08-13 19:51:36 +0200

    add page register

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,57 @@
+import { Button, Container, TextInput, PasswordInput, Title } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import api from '../services/api';
+
+export default function Register() {
+    const form = useForm({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+            password: '',
+        });
+
+        const handleSubmit = async (values: typeof form.values) => {
+            await api.post('/auth/signup', values);
+            alert("Inscription réussie !");
+        };
+
+    return (
+        <Container size={420} my={40}>
+            <Title>Inscription</Title>
+            <form onSubmit={form.onSubmit(handleSubmit)}>
+                <TextInput
+                    label="Prénom"
+                    placeholder="Votre prénom"
+                    {...form.getInputProps('firstName')}
+                />
+                <TextInput
+                    label="Nom"
+                    placeholder="Votre nom"
+                    mt="md"
+                    {...form.getInputProps('lastName')}
+                />
+                <TextInput
+                    label="Email"
+                    placeholder="Votre email"
+                    mt="md"
+                    {...form.getInputProps('email')}
+                />
+                <TextInput
+                    label="Téléphone"
+                    placeholder="Votre téléphone"
+                    mt="md"
+                    {...form.getInputProps('phone')}
+                />
+                <PasswordInput
+                    label="Mot de passe"
+                    placeholder="Votre mot de passe"
+                    mt="md"
+                    {...form.getInputProps('password')}
+                />
+                <Button fullWidth mt="xl" type="submit">S'inscrire</Button>
+            </form>
+        </Container>
+    );
+}
