67fec6e19001384c3184e657b1a8f23aa69c052d
Raiser00 <damien.khashan@ifosup.wavre.be>
2025-08-13 08:08:14 +0200

    add some function for test

--- backend/controllers/market.controller.js
+++ backend/controllers/market.controller.js
@@ -5,3 +5,23 @@ exports.create = async (req, res) => {
     res.status(201).json(market);
 };
 
+exports.getAll = async (req, res) => {
+    const markets = await Market.find();
+    res.json(markets);
+};
+
+exports.getOpen = async (req, res) => {
+    const now = new Date();
+    const markets = await Market.find({ 'applicationPeriod.end': { $gt: now } });
+    res.json(markets);
+};
+
+exports.update = async (req, res) => {
+    await Market.findByIdAndUpdate(req.params.id, req.body);
+    res.json({ message: 'Marché mis à jour' });
+};
+
+exports.remove = async (req, res) => {
+    await Market.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Marché supprimé' });
+};
