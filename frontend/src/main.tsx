7f262a87013159703b350fcd5f9838243521e809
Raiser00 <damien.khashan@ifosup.wavre.be>
2025-08-17 17:09:23 +0200

    update mantine

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <MantineProvider>
     <App />
-  </React.StrictMode>
+  </MantineProvider>
 );
